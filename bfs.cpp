d6512cf05a44665e9dd17c09892a3628bbba6843
Egor <egor-syrov@bk.ru>
2025-04-06 09:45:07 +0300

    woking maze bfs

--- bfs.cpp
+++ bfs.cpp
@@ -33,9 +33,8 @@ unsigned int bfs(std::span<const std::vector<std::size_t>> adj, std::size_t sour
     return std::count(visited.begin(), visited.end(), true);
 }
 
-std::optional<uint16_t> bfs(std::span<const std::vector<uint8_t>> maze,
-                               const std::pair<std::size_t, std::size_t>& start,
-                               const std::pair<std::size_t, std::size_t>& finish)
+std::optional<uint16_t> bfs(std::span<std::vector<uint8_t>> maze, const std::pair<std::size_t, std::size_t> start,
+                            const std::pair<std::size_t, std::size_t> finish)
 {
     std::size_t n = maze.size();
     if (n == 0) return std::nullopt;
@@ -45,8 +44,8 @@ std::optional<uint16_t> bfs(std::span<const std::vector<uint8_t>> maze,
     q.push(start);
 
     constexpr const int dx[] = {-1, 1, 0, 0};
-    constexpr const int dy[] = {0, 0, -1, -1};
-    std::vector<std::vector<bool>> visited(n, std::vector<bool>(m, -1));
+    constexpr const int dy[] = {0, 0, -1, 1};
+    std::vector<std::vector<bool>> visited(n, std::vector<bool>(m, false));
     visited[start.first][start.second] = true;
     uint16_t steps = 0;
 
@@ -57,11 +56,12 @@ std::optional<uint16_t> bfs(std::span<const std::vector<uint8_t>> maze,
         if (current == finish) return steps;
         for (std::size_t i = 0; i < 4; ++i)
         {
-            int nx = current.first + dx[i];
-            int ny = current.second + dy[i];
+            std::size_t nx = current.first + dx[i];
+            std::size_t ny = current.second + dy[i];
 
-            if (nx < m && ny < n && !visited[ny][nx] && maze[nx][ny] != 1)
+            if (nx < m && ny < n && !visited[ny][nx] && maze[ny][nx] != 1)
             {
+                maze[ny][nx] = 2;
                 visited[ny][nx] = true;
                 q.emplace(nx, ny);
             }
@@ -73,33 +73,46 @@ std::optional<uint16_t> bfs(std::span<const std::vector<uint8_t>> maze,
 
 int main()
 {
-    std::size_t n, m;
-    std::cout << "Enter number of row (n > 0).\n";
-    if (!(std::cin >> n) || n <= 0) throw std::invalid_argument("Invalid value for N: must be integer > 0.");
-    std::cout << "Enter number of columns (m > 0).\n";
-    if (!(std::cin >> m) || m <= 0) throw std::invalid_argument("Invalid value for M: must be integer > 0.");
-
-    std::vector<std::vector<uint8_t>> maze(n, std::vector<uint8_t>(m));
-
-    for (std::size_t i = 0; i < n; ++i)
+    try
     {
-        for (std::size_t j = 0; j < m; ++j)
+        std::size_t n, m;
+        std::cout << "Enter number of row (n > 0).\n";
+        if (!(std::cin >> n) || n <= 0) throw std::invalid_argument("Invalid value for N: must be integer > 0.");
+        std::cout << "Enter number of columns (m > 0).\n";
+        if (!(std::cin >> m) || m <= 0) throw std::invalid_argument("Invalid value for M: must be integer > 0.");
+
+        std::vector<std::vector<uint8_t>> maze(n, std::vector<uint8_t>(m));
+
+        std::cout << "Enter maze (1 - wall, 0 - empty field.\n";
+        for (std::size_t i = 0; i < n; ++i)
         {
-            if (!(std::cin >> maze[i][j])) throw std::invalid_argument("Invalid maze`s element.");
+            for (std::size_t j = 0; j < m; ++j)
+            {
+                if (!(std::cin >> maze[i][j])) throw std::invalid_argument("Invalid maze`s element.");
+            }
+        }
+
+        auto path_length = bfs(maze, {0, 0}, {n - 1, m - 1});
+
+        if (!path_length)
+        {
+            std::cerr << "Path not exists.\n";
+            return EXIT_FAILURE;
+        }
+
+        std::cout << "\nPath length: " << *path_length << "\n";
+        for (auto const& line : maze)
+        {
+            for (auto const el : line)
+            {
+                std::cout << el << " ";
+            }
+            std::cout << "\n";
         }
     }
-
-    auto path_length = bfs(maze, {0, 0}, {n - 1, m - 1});
-
-    if (!path_length) throw std::runtime_error("Path not exists.");
-
-    for (auto const& line : maze)
+    catch (const std::exception& e)
     {
-        for (auto const el : line)
-        {
-            std::cout << el << " ";
-        }
-        std::cout << "\n";
+        std::cerr << "Catched error: " << e.what() << "\n";
     }
-    return 0;
+    return EXIT_SUCCESS;
 }

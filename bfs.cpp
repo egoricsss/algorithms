69c09907ad9652aca66d1d1154ece2b2a17d7ff6
Egor <egor-syrov@bk.ru>
2025-04-06 08:17:47 +0300

    debug next time

--- bfs.cpp
+++ bfs.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cstdint>
 #include <iostream>
 #include <optional>
 #include <queue>
@@ -32,10 +33,12 @@ unsigned int bfs(std::span<const std::vector<std::size_t>> adj, std::size_t sour
     return std::count(visited.begin(), visited.end(), true);
 }
 
-std::optional<std::size_t> bfs(std::span<const std::vector<int>> maze, const std::pair<std::size_t, std::size_t>& start,
+std::optional<uint16_t> bfs(std::span<const std::vector<uint8_t>> maze,
+                               const std::pair<std::size_t, std::size_t>& start,
                                const std::pair<std::size_t, std::size_t>& finish)
 {
     std::size_t n = maze.size();
+    if (n == 0) return std::nullopt;
     std::size_t m = maze[0].size();
 
     std::queue<std::pair<std::size_t, std::size_t>> q;
@@ -43,27 +46,27 @@ std::optional<std::size_t> bfs(std::span<const std::vector<int>> maze, const std
 
     constexpr const int dx[] = {-1, 1, 0, 0};
     constexpr const int dy[] = {0, 0, -1, -1};
-    std::vector<std::vector<int>> dist(n, std::vector<int>(m, -1));
-    // -1 - means, that not visited,
-    // 1 - wall,  0 - visited
-    dist[start.first][start.second] = 0;
+    std::vector<std::vector<bool>> visited(n, std::vector<bool>(m, -1));
+    visited[start.first][start.second] = true;
+    uint16_t steps = 0;
 
     while (!q.empty())
     {
         auto current = q.front();
         q.pop();
-        if (current == finish) return dist[current.first][current.second];
+        if (current == finish) return steps;
         for (std::size_t i = 0; i < 4; ++i)
         {
             int nx = current.first + dx[i];
             int ny = current.second + dy[i];
 
-            if (nx >= 0 && nx < m && ny >= 0 && ny < n && maze[ny][nx] != 1 && maze[ny][nx] == -1)
+            if (nx < m && ny < n && !visited[ny][nx] && maze[nx][ny] != 1)
             {
-                dist[ny][nx] += dist[current.first][current.second] + 1;
-                q.push({nx, ny});
+                visited[ny][nx] = true;
+                q.emplace(nx, ny);
             }
         }
+        ++steps;
     }
     return std::nullopt;
 }
@@ -76,7 +79,7 @@ int main()
     std::cout << "Enter number of columns (m > 0).\n";
     if (!(std::cin >> m) || m <= 0) throw std::invalid_argument("Invalid value for M: must be integer > 0.");
 
-    std::vector<std::vector<int>> maze(n, std::vector<int>(m));
+    std::vector<std::vector<uint8_t>> maze(n, std::vector<uint8_t>(m));
 
     for (std::size_t i = 0; i < n; ++i)
     {
